0bd76c6060012e6726983196a22864d86e49774d
SourDoughCrumb <109032333+SourDoughCrumb@users.noreply.github.com>
2022-09-27 19:14:29 +0200

    First OG commit, added game ID

--- test_1.py
+++ test_1.py
@@ -4,6 +4,7 @@ import pprint
 
 #let's try to get the response 1st test whats up dawg?
 response = requests.get("https://api.opendota.com/api/matches/6776595085")
-
+response2 = requests.get("https://api.opendota.com/api/matches/6778264672")
 
 pprint.pprint(response.json())
+pprint.pprint(response2.json())
