1301e6aa3a32195eeacac43ac8336d369f70da97
rbekeris <bekeris.code@gmail.com>
2022-10-05 19:28:57 +0200

    added more pages

---
+++ Dash_app_0_1/Webapp/Bakerapp/main/views.py
@@ -0,0 +1,29 @@
+from flask import render_template
+from . import main
+import plotly
+import plotly.graph_objs as go
+import pandas as pd
+import numpy as np
+import json
+
+def create_plot()
+    df = 
+
+    trace_1 = go.Scatter(
+                        x=(1,2,3),
+                        y=(1,2,3),
+                        mode='lines+markers',
+                        name ='Player_1')
+    trace_2 = go.Scatter(
+                        x=(1,2,3),
+                        y=(2,3,4),
+                        mode='lines+markers',
+                        name = 'Player_2')                 
+    data = [trace_1, trace_2]
+    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphJSON
+@main.route('/')
+
+def index():
+    bar = create_plot()
+    return render_template('index.html', plot = bar)
