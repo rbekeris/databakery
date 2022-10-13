4196d28d19fef3a7f0edfa1c9c000d706be33f56
SourDoughCrumb <109032333+SourDoughCrumb@users.noreply.github.com>
2022-10-13 18:09:04 +0200

    Finalized code for Plotly of GPM over time

--- date_gpm.py
+++ date_gpm.py
@@ -1,12 +1,18 @@
 import json
+import datetime
+import os
 
 date_gpm = {}
 
-with open('Sample_Matches/6779631970.txt') as f:
-    data = json.load(f)
-    date = data['start_time']
-    for i in range(0,len(data['players'])):
-        if data['players'][i]['account_id'] == 872190418:
-            gpm = data['players'][i]['gold_per_min']
-    date_gpm[date] = gpm
-    print(date_gpm)
+for filename in os.listdir('Sample_Matches'):
+    with open(str('Sample_Matches/'+filename)) as f:
+        data = json.load(f)
+        date_ep = data['start_time']
+        type(date_ep)
+        date = datetime.datetime.fromtimestamp(date_ep).strftime('%Y-%m-%d %H:%M:%S')
+        for i in range(0,len(data['players'])):
+            if data['players'][i]['account_id'] == 1163336706:
+                gpm = data['players'][i]['gold_per_min']
+        date_gpm[date] = gpm
+
+print(date_gpm)
