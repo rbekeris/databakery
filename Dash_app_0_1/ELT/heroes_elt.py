90958f7d4f5ce2b90aa5ce7410be2e613227216a
rbekeris <bekeris.code@gmail.com>
2022-10-16 14:19:15 +0200

    heroes ELT works

--- Dash_app_0_1/ELT/heroes_elt.py
+++ Dash_app_0_1/ELT/heroes_elt.py
@@ -7,13 +7,19 @@ import os
 from sqlalchemy import Table, Column, Integer, String
 from sqlalchemy import MetaData
 from sqlalchemy import insert
+from sqlalchemy.dialects import postgresql
 
 load_dotenv()
 db_connection_string = os.getenv('SQLALCHEMY_DATABASE_URL_FROM_OUTSIDE_CONTAINER_NETWORK')
 engine = create_engine(db_connection_string)
 heroes_table = Table("heroes",
                     MetaData(),
-                    Column(),
+                    Column("id",Integer, primary_key =True),
+                    Column("name",String),
+                    Column("localized_name",String),
+                    Column("primary_attr",String),
+                    Column("attack_type",String),
+                    Column("roles",Integer, postgresql.ARRAY(String)),
                     schema="core_schema")
 
 heroes = requests.get("https://api.opendota.com/api/heroes").json()
@@ -24,36 +30,13 @@ for hero in heroes:
     localized_name = hero['localized_name']
     primary_attr = hero['primary_attr']
     attack_type = hero['attack_type']
-    #roles = hero['roles']
+    roles = hero['roles']
 
     stmt = stmt = insert(heroes_table).values(id = id,
                                                     name = name, 
                                                     localized_name = localized_name,
                                                     primary_attr = primary_attr,
-                                                    attack_type = attack_type)
+                                                    attack_type = attack_type,
+                                                    roles = roles)
     with engine.connect() as conn:
         result = conn.execute(stmt)
-        conn.commit()
-
-    # insert_statemen = '''INSERT INTO core_schema.heroes (id,
-    #                                                 name,
-    #                                                 localized_name,
-    #                                                 primary_attr,
-    #                                                 attack_type)
-    #                 VALUES ({id},
-    #                         '{name}',
-    #                         '{localized_name}',
-    #                         '{primary_attr}',
-    #                         '{attack_type}'
-    #                         )
-    #                 '''.format(
-    #                     id = id,
-    #                     name = name, 
-    #                     localized_name = localized_name,
-    #                     primary_attr = primary_attr,
-    #                     attack_type = attack_type
-    #                 )
-    # try:
-    #     engine.execute(insert_statemen)
-    # except:
-    #     print("something went wrong with {hero}".format(hero = localized_name))
